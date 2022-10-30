8c6ef3f4a287ea8ffd3547679dcfe1688aa6b6b8
Administrator <fearlessquad0@gmail.com>
2022-10-30 23:45:33 +0500

    first comme

---
+++ build/tmp/kapt3/stubs/debug/com/infusiblecoder/allinonevideodownloader/utils/Constants.java
@@ -0,0 +1,94 @@
+package com.infusiblecoder.allinonevideodownloader.utils;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0018\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u000b\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0006\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J*\u0010(\u001a\u00020)2\u0006\u0010*\u001a\u00020+2\b\u0010,\u001a\u0004\u0018\u00010\u00042\b\u0010-\u001a\u0004\u0018\u00010\u00042\u0006\u0010.\u001a\u00020#J\u0016\u0010/\u001a\u00020)2\u0006\u0010*\u001a\u00020+2\u0006\u00100\u001a\u00020\u0004R\u000e\u0010\u0003\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\b\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\t\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\n\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000b\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\f\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\r\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000e\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000f\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0010\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0011\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0012\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0013\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u001c\u0010\u0014\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0015\u0010\u0016\"\u0004\b\u0017\u0010\u0018R\u001c\u0010\u0019\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001a\u0010\u0016\"\u0004\b\u001b\u0010\u0018R\u001a\u0010\u001c\u001a\u00020\u001dX\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001e\u0010\u001f\"\u0004\b \u0010!R\u000e\u0010\"\u001a\u00020#X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010$\u001a\u00020#X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010%\u001a\u00020#X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010&\u001a\u00020#X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\'\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u00061"}, d2 = {"Lcom/infusiblecoder/allinonevideodownloader/utils/Constants;", "", "()V", "DlApisUrl", "", "DlApisUrl2", "DlApisUrl3", "FOLDER_NAME", "FOLDER_NAME_Whatsapp_and11", "FOLDER_NAME_Whatsapp_and11_B", "FOLDER_NAME_Whatsappbusiness", "MY_ANDROID_10_IDENTIFIER_OF_FILE", "PREF_APPNAME", "PREF_CLIP", "SAVE_FOLDER_NAME", "STARTFOREGROUND_ACTION", "STOPFOREGROUND_ACTION", "directoryInstaShoryDirectorydownload_audio", "directoryInstaShoryDirectorydownload_images", "directoryInstaShoryDirectorydownload_videos", "myPhotoUrlIs", "getMyPhotoUrlIs", "()Ljava/lang/String;", "setMyPhotoUrlIs", "(Ljava/lang/String;)V", "myVideoUrlIs", "getMyVideoUrlIs", "setMyVideoUrlIs", "myprogressDD", "Landroid/app/ProgressDialog;", "getMyprogressDD", "()Landroid/app/ProgressDialog;", "setMyprogressDD", "(Landroid/app/ProgressDialog;)V", "show_Ads", "", "show_earning_card_in_extrafragment", "showsoundcloud", "showyoutube", "tiktokWebviewUrl", "downloadInstagramImageOrVideodata", "", "context", "Landroid/content/Context;", "URL", "Cookie", "isLog", "startInstaDownload", "Url", "app_debug"})
+public final class Constants {
+    @org.jetbrains.annotations.NotNull()
+    public static final com.infusiblecoder.allinonevideodownloader.utils.Constants INSTANCE = null;
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String DlApisUrl3 = "https://iphoting-yt-dl-api.herokuapp.com/api/info?url=";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String DlApisUrl2 = "https://dlphpapis.herokuapp.com/api/info?url=";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String DlApisUrl = "https://dlphpapis21.herokuapp.com/api/info?url=";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String STARTFOREGROUND_ACTION = "com.infusiblecoder.allinonevideodownloader.action.startforeground";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String STOPFOREGROUND_ACTION = "com.infusiblecoder.allinonevideodownloader.action.stopforeground";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String PREF_APPNAME = "aiovidedownloader";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String PREF_CLIP = "tikVideoDownloader";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String FOLDER_NAME = "/WhatsApp/";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String FOLDER_NAME_Whatsappbusiness = "/WhatsApp Business/";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String FOLDER_NAME_Whatsapp_and11 = "/Android/media/com.whatsapp/WhatsApp/";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String FOLDER_NAME_Whatsapp_and11_B = "/Android/media/com.whatsapp.w4b/WhatsApp Business/";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String SAVE_FOLDER_NAME = "/Download/AIO_Status_Saver/";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String MY_ANDROID_10_IDENTIFIER_OF_FILE = "All_Video_Downloader_";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String tiktokWebviewUrl = "https://www.tiktok.com/?lang=en";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String directoryInstaShoryDirectorydownload_videos = "/InstaStory/videos/";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String directoryInstaShoryDirectorydownload_images = "/InstaStory/images/";
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String directoryInstaShoryDirectorydownload_audio = "/InstaStory/audios/";
+    public static final boolean showyoutube = false;
+    public static final boolean showsoundcloud = false;
+    public static final boolean show_Ads = true;
+    public static final boolean show_earning_card_in_extrafragment = true;
+    @org.jetbrains.annotations.Nullable()
+    private static java.lang.String myVideoUrlIs = "";
+    @org.jetbrains.annotations.Nullable()
+    private static java.lang.String myPhotoUrlIs = "";
+    public static android.app.ProgressDialog myprogressDD;
+    
+    private Constants() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getMyVideoUrlIs() {
+        return null;
+    }
+    
+    public final void setMyVideoUrlIs(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getMyPhotoUrlIs() {
+        return null;
+    }
+    
+    public final void setMyPhotoUrlIs(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final android.app.ProgressDialog getMyprogressDD() {
+        return null;
+    }
+    
+    public final void setMyprogressDD(@org.jetbrains.annotations.NotNull()
+    android.app.ProgressDialog p0) {
+    }
+    
+    public final void startInstaDownload(@org.jetbrains.annotations.NotNull()
+    android.content.Context context, @org.jetbrains.annotations.NotNull()
+    java.lang.String Url) {
+    }
+    
+    public final void downloadInstagramImageOrVideodata(@org.jetbrains.annotations.NotNull()
+    android.content.Context context, @org.jetbrains.annotations.Nullable()
+    java.lang.String URL, @org.jetbrains.annotations.Nullable()
+    java.lang.String Cookie, boolean isLog) {
+    }
+}
